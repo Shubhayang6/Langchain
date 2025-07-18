8cf9dce87810bb92f414772ce842fd156f135231
Shubhayan Ghosh <shubhayan.tech@gmail.com>
2025-07-19 00:40:04 +0530

    Add Simple chatbot with terminal

---
+++ chatbot.py
@@ -0,0 +1,33 @@
+import getpass
+import os
+
+try:
+    from dotenv import load_dotenv
+    
+    load_dotenv()
+except ImportError:
+    pass
+
+
+if not os.environ.get('MISTRAL_API_KEY'):
+    os.environ['MISTRAL_API_KEY'] = getpass.getpass("Enter the API KEY for MISTRAL AI:")
+    
+from langchain.chat_models import init_chat_model
+
+model = init_chat_model('mistral-large-latest', model_provider = 'mistralai')
+
+model.invoke('Hello, world!')
+print("DOnE")
+
+from langchain_core.messages import HumanMessage, SystemMessage
+
+messages = [
+    SystemMessage("Answer the question based on best on your knowledge"),
+    HumanMessage("When was the last data uploaded to you as a model?")
+]
+
+# Streaming
+
+for token in model.stream(messages):
+    print(token.content, end = '', flush = True)
+# model.invoke(messages)
